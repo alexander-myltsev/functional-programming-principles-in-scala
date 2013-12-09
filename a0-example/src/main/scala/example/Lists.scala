6190339f34a06cac412afb7a4d18934970449774
Alexander Myltsev <alexander.myltsev@phystech.edu>
2013-12-10 03:15:13 +0400

    Solve 0th assignment

--- a0-example/src/main/scala/example/Lists.scala
+++ a0-example/src/main/scala/example/Lists.scala
@@ -1,6 +1,7 @@
 package example
 
 import common._
+import scala.collection.immutable.Nil
 
 object Lists {
   /**
@@ -23,7 +24,10 @@ object Lists {
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = xs match {
+    case Nil => 0
+    case x :: xs => x + sum(xs)
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -38,5 +42,9 @@ object Lists {
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = xs match {
+    case Nil => throw new java.util.NoSuchElementException()
+    case x :: Nil => Int.MinValue
+    case x :: xs => math.max(x, max(xs))
+  }
 }
--- a0-example/src/test/scala/example/ListsSuite.scala
+++ a0-example/src/test/scala/example/ListsSuite.scala
@@ -46,8 +46,8 @@ class ListsSuite extends FunSuite {
    *
    * This allows tests to be written in a more readable manner:
    */
-  test("one plus one is three?") {
-    assert(1 + 1 == 3) // This assertion fails! Go ahead and fix it.
+  test("one plus two is three") {
+    assert(1 + 2 == 3) // This assertion fails! Go ahead and fix it.
   }
 
 
@@ -72,7 +72,7 @@ class ListsSuite extends FunSuite {
    * We recommend to always use the `===` equality operator when writing tests.
    */
   test("details why one plus one is not three") {
-    assert(1 + 1 === 3) // Fix me, please!
+    assert(1 + 2 === 3) // Fix me, please!
   }
 
 
@@ -120,5 +120,11 @@ class ListsSuite extends FunSuite {
   
   test("max of a few numbers") {
     assert(max(List(3, 7, 2)) === 7)
+  }
+  
+  test("no max in empty list") {
+    intercept[java.util.NoSuchElementException] {
+      max(List())
+    }
   }
 }
