5499f32d282af61f69dec26bf48aad7bcb80999a
Alexander Myltsev <alexander.myltsev@phystech.edu>
2013-12-11 03:51:31 +0400

    Solve 1st assignment

--- a1-recfun/src/main/scala/recfun/Main.scala
+++ a1-recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,35 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(column: Int, row: Int): Int = (column, row) match {
+    case (c, r) if r < c => throw new IllegalArgumentException("must be: r >= c")
+    case (c, r) if (column == 0 || column == row) => 1
+    case _ => pascal(column, row - 1) + pascal(column - 1, row - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def parenthesesCounter(cs: List[Char], leftParenthesesCount: Int): Boolean =
+      if (leftParenthesesCount < 0) false
+      else cs match {
+        case Nil => leftParenthesesCount == 0
+        case ')' :: xs => parenthesesCounter(xs, leftParenthesesCount - 1)
+        case '(' :: xs => parenthesesCounter(xs, leftParenthesesCount + 1)
+        case x :: xs => parenthesesCounter(xs, leftParenthesesCount)
+      }
+    parenthesesCounter(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeWays(money: Int, coins: List[Int]): Int =
+      if (money == 0) 1
+      else if (coins.isEmpty || money < 0) 0
+      else countChangeWays(money, coins.tail) + countChangeWays(money - coins.head, coins)
+    countChangeWays(money, coins.sorted)
+  }
 }
--- a1-recfun/src/test/scala/recfun/PascalSuite.scala
+++ a1-recfun/src/test/scala/recfun/PascalSuite.scala
@@ -9,14 +9,20 @@ import org.scalatest.junit.JUnitRunner
 class PascalSuite extends FunSuite {
   import Main.pascal
   test("pascal: col=0,row=2") {
-    assert(pascal(0,2) === 1)
+    assert(pascal(0, 2) === 1)
   }
 
   test("pascal: col=1,row=2") {
-    assert(pascal(1,2) === 2)
+    assert(pascal(1, 2) === 2)
   }
 
   test("pascal: col=1,row=3") {
-    assert(pascal(1,3) === 3)
+    assert(pascal(1, 3) === 3)
+  }
+
+  test("pascal: row < col") {
+    intercept[IllegalArgumentException] {
+      pascal(3, 1)
+    }
   }
 }
